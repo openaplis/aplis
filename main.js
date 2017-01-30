e9f02d060d88034fdac066eedd95ef6693fa4335
SidHarder <softwarenavigator@gmail.com>
2017-01-30 16:42:29 -0700

    adding initial files

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        frame: false,
+        height: 700,
+        resizable: false,
+        width: 368
+    });
+
+    mainWindow.loadUrl('file://' + __dirname + '/src/index.html');
+});
